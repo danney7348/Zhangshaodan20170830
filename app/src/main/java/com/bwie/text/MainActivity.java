04c9d4e84c86b389007ef8592782ae0e7cd6d356
danney7348 <31384817+danney7348@users.noreply.github.com>
2017-08-30 15:18:20 +0800

    as

--- app/src/main/java/com/bwie/text/MainActivity.java
+++ app/src/main/java/com/bwie/text/MainActivity.java
@@ -3,10 +3,15 @@ package com.bwie.text;
 import android.content.ComponentCallbacks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.bwie.text.adapter.MyApadter;
 import com.bwie.text.bean.News;
+import com.bwie.text.fragment.LeftFragment;
+import com.bwie.text.fragment.RightFragment;
 import com.google.gson.Gson;
+import com.kson.slidingmenu.SlidingMenu;
+import com.kson.slidingmenu.app.SlidingFragmentActivity;
 
 import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
@@ -22,19 +27,33 @@ import view.xlistview.XListView;
  * 实现新闻的页面
  */
 @ContentView(R.layout.activity_main)
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @ViewInject(R.id.lv)
     XListView lv;
     private String url = "http://v.juhe.cn/toutiao/index";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //使用每个控件的声明和初始化必须用IOC反转控制，通过注解的形式初始化View
         x.view().inject(this);
         //使用post请求
         initPost();
+        initMenu();
+    }
+
+    private void initMenu() {
+        setBehindContentView(R.layout.left_fragment_content);
+        getSupportFragmentManager().beginTransaction().replace(R.id.left_fl1, new LeftFragment()).commit();
+
+        SlidingMenu menu = getSlidingMenu();
+        menu.setMode(SlidingMenu.LEFT_RIGHT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        menu.setBehindOffsetRes(R.dimen.BehindOffsetRes);
+
+        menu.setSecondaryMenu(R.layout.right_fragment_content);
+        getSupportFragmentManager().beginTransaction().replace(R.id.left_fl2,new RightFragment()).commit();
     }
 
     /**
---
+++ app/src/main/java/com/bwie/text/fragment/RightFragment.java
@@ -0,0 +1,31 @@
+package com.bwie.text.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bwie.text.R;
+
+/**
+ * 作者： 张少丹
+ * 时间：  2017/8/30.
+ * 邮箱：1455456581@qq.com
+ * 类的用途：
+ */
+
+public class RightFragment extends Fragment {
+    private View mRootView;
+    @Nullable
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        if(mRootView == null){
+            mRootView = View.inflate(getContext(), R.layout.fragment2_item,null);
+        }
+        return mRootView;
+    }
+}
